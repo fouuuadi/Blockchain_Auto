306df380eb2257f54adf8226983c1dca21eaed70
fouuuadi <fouadlamnaouar18@gmail.com>
2025-03-07 11:59:21 +0100

    ajout function buy cars

--- blockchain_app/src/app/blockchain/page.tsx
+++ blockchain_app/src/app/blockchain/page.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-// import Image from "next/image";
-// import {HETIC_ABI} from '../../../public/Hetic';
-// import { ConnectButton } from '@rainbow-me/rainbowkit';
-// import { useAccount, useWriteContract, useReadContract } from 'wagmi';
 import Footer from '../Footer/page';
 import Navbar from '../Navbar/page';
-/**
- * Adresse du contrat HETIC ERC20.
- * Cette adresse est utilisée pour interagir avec le contrat intelligent HETIC.
- */
+import Card from '../Card/page';
 
 export default function Blockchain() {
    
@@ -17,6 +10,7 @@ export default function Blockchain() {
   return (
     <div>
       <Navbar></Navbar>
+      <Card></Card>
        <Footer></Footer>
     </div>
   );
